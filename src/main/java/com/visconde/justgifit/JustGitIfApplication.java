d28cf57354e3a89e062fca70f34210828a514b89
Visconde <renan.g.visconde@accenture.com>
2017-10-13 14:10:32 -0300

    first commit

---
+++ src/main/java/com/visconde/justgifit/JustGitIfApplication.java
@@ -0,0 +1,12 @@
+package com.visconde.justgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGitIfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGitIfApplication.class, args);
+	}
+}
